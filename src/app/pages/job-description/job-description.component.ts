841ff705f91505ddb71c429a2be2df7f37e1076e
Oto Machavariani <machavarian10to>
2023-02-21 18:43:03 +0400

    feat: make job description page

--- src/app/pages/job-description/job-description.component.ts
+++ src/app/pages/job-description/job-description.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JobService } from 'src/app/services/job.service';
+
 
 @Component({
   selector: 'app-job-description',
   templateUrl: './job-description.component.html',
-  styleUrls: ['./job-description.component.scss']
+  styleUrls: ['./job-description.component.scss'],
 })
-export class JobDescriptionComponent {
+export class JobDescriptionComponent implements OnInit {
+  id: any;
+  job: any;
 
+  constructor(private route: ActivatedRoute, private jobService: JobService) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.jobService.getJobById(this.id).subscribe(data => {
+      this.job = data;
+    });
+  }
 }
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,11 @@ export class JobService {
 
   getJobs(): Observable<any>{
     return this.http.get(`${this.apiUrl}&q=developer&start=0&gl=us`)
+  }
+
+  getJobById(id: any): Observable<any> {
+    return this.getJobs().pipe(
+      map((job: { jobs_results: any[]; }) => job.jobs_results.find((j: { job_id: any; }) => j.job_id === id))
+    );
   }
 }
